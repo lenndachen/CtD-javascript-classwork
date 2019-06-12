19cff9eb5b5be1df60326c523c55ced2bbbf51a2
Lennda Chen <lindachen548@gmail.com>
2019-06-12 19:57:37 -0400

    h

--- math.js
+++ math.js
@@ -1,17 +1,5 @@
-function add(a, b) {
+function math(a, b) {
     return a + b;
 }
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-module.exports = {
-    add: add,
-    subtract: subtract,
-    multiply: multiply
-};
+module.exports = math.js;
